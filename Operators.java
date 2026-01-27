003ffafab6ba124bc2730a6e386503025ec47be6
TANAY PANDEY <tanaypandey901@gmail.com>
2026-01-27 22:16:06 +0530

    Assignment-3

---
+++ Operators.java
@@ -0,0 +1,12 @@
+public class Operators{
+public static void main(String[] args){
+for (int i=0;i<10;i++){
+system.out.println("i: " +i);
+
+int j=10;
+
+while(j>0){
+system.out.println("j: " +j);
+}
+}
+}
