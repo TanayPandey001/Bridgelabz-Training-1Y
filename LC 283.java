6be583c9b27364ed0efaae2d36d29f04bc5ed02f
TANAY PANDEY <tanaypandey901@gmail.com>
2026-02-03 23:42:39 +0530

    first commit

---
+++ LC 283.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int t=0;
+        for(int i=0; i<nums.length; i++){
+           for(int j=i; j<nums.length-1; j++){
+                if(nums[j]==0){
+                    t=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=t;
+                }
+            }
+        }
+    }
+}
