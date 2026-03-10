9d18b6d10dd8d042733fa25f7de71ea14526363f
TANAY PANDEY <tanaypandey901@gmail.com>
2026-03-10 23:54:17 +0530

    Abstraction

---
+++ src/com/gla/Main.java
@@ -0,0 +1,48 @@
+package com.gla;
+
+public interface SmartDevice {
+    void turnOn();
+    void turnOff();
+}
+
+class Light implements SmartDevice {
+    public void turnOn() {
+        System.out.println("Light ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Light OFF");
+    }
+}
+
+class AC implements SmartDevice {
+    public void turnOn() {
+        System.out.println("AC ON");
+    }
+
+    public void turnOff() {
+        System.out.println("AC OFF");
+    }
+}
+
+class TV implements SmartDevice {
+    public void turnOn() {
+        System.out.println("TV ON");
+    }
+
+    public void turnOff() {
+        System.out.println("TV OFF");
+    }
+}
+
+class SmartHome {
+    public static void main(String[] args) {
+        SmartDevice l = new Light();
+        SmartDevice a = new AC();
+        SmartDevice t = new TV();
+
+        l.turnOn();
+        a.turnOn();
+        t.turnOn();
+    }
+}
